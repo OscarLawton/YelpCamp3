252fb690bbb98c63cd84f8aa83bdd852ee1b0b75
Emmet Lawton <Emmet@Emmets-MacBook-Pro.local>
2021-02-21 15:12:30 +0000

    add JOI

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            location: `${cities[random1000].city},${cities[random1000].state}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
             description: 'All the lorem ispsum and sit amet sonsectuer addipsicn and al that good stuff. Just give it time it will come. Relax',
