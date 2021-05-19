bc5f930fd8139e5e1b4e69cd120d0583e25bb164
Oscar Lawton <emmetlawton@gmail.com>
2021-05-19 01:56:10 +0100

    video 527 done

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,10 +4,16 @@ const campgrounds = require('../controllers/campgrounds')
 const catchAsync = require('../utils/catchAsync')
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 const { reset } = require('nodemon')
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
 
 router.get('/', catchAsync(campgrounds.index))
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCamp))
+//router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCamp))
+
+router.post('/', upload.single('image'), (req, res) =>{
+    res.send(req.body, req.file)
+})
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
