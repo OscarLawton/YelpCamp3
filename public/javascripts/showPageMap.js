4a63cde923f748b98edcf22f8eabb67b841c16a4
Oscar Lawton <emmetlawton@gmail.com>
2021-05-27 04:47:54 +0100

    video 544 done

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,13 @@
+//const Campground = require("../../models/Campground");
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-74.5, 40],
-    zoom: 9
+    center: campground.geometry.coordinates,
+    zoom: 7
 });
+
+new mapboxgl.Marker()
+    .setLngLat([-74.5, 40])
+    .addTo(map)
